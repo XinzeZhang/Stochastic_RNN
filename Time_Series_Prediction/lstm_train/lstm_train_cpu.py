1da2a9f0d438af0b4dc7463cbfee4ae53f924bc0
XinzeZhang <xinze.zh@outlook.com>
2018-02-05 16:46:38 +0800

    added gpu versio

--- Time_Series_Prediction/lstm_train/lstm_train_cpu.py
+++ Time_Series_Prediction/lstm_train/lstm_train_cpu.py
@@ -8,26 +8,30 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+import time
+
 class Sequence(nn.Module):
     def __init__(self):
         super(Sequence, self).__init__()
-        self.gru1 = nn.GRUCell(1, 51)
-        self.gru2 = nn.GRUCell(51, 51)
+        self.lstm1 = nn.LSTMCell(1, 51)
+        self.lstm2 = nn.LSTMCell(51, 51)
         self.linear = nn.Linear(51, 1)
 
     def forward(self, input, future = 0):
         outputs = []
         h_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        c_t = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
         h_t2 = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
+        c_t2 = Variable(torch.zeros(input.size(0), 51).double(), requires_grad=False)
 
         for i, input_t in enumerate(input.chunk(input.size(1), dim=1)):
-            h_t= self.gru1(input_t, h_t)
-            h_t2 = self.gru2(h_t, h_t2)
+            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
             output = self.linear(h_t2)
             outputs += [output]
         for i in range(future):# if we should predict the future
-            h_t= self.gru1(output, h_t)
-            h_t2 = self.gru2(h_t, h_t2)
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
             output = self.linear(h_t2)
             outputs += [output]
         outputs = torch.stack(outputs, 1).squeeze(2)
@@ -36,6 +40,9 @@ class Sequence(nn.Module):
 
 
 if __name__ == '__main__':
+    # Initialize timer
+    time_tr_start=time.time()
+
     # set random seed to 0
     np.random.seed(0)
     torch.manual_seed(0)
@@ -51,6 +58,9 @@ if __name__ == '__main__':
     criterion = nn.MSELoss()
     # use LBFGS as optimizer since we can load the whole data to train
     optimizer = optim.LBFGS(seq.parameters(), lr=0.8)
+
+    print("Using CPU i7-7700k! \n")
+    print("--- Training LSTM ---")
     #begin to train
     for i in range(15):
         print('STEP: ', i)
@@ -58,7 +68,9 @@ if __name__ == '__main__':
             optimizer.zero_grad()
             out = seq(input)
             loss = criterion(out, target)
-            print('loss:', loss.data.numpy()[0])
+            # record train time
+            training_time = time.time()-time_tr_start
+            print('MSE: %.10f \t Total time: %.3f' % (loss.data.numpy()[0], training_time))
             loss.backward()
             return loss
         optimizer.step(closure)
@@ -67,19 +79,19 @@ if __name__ == '__main__':
         pred = seq(test_input, future = future)
         loss = criterion(pred[:, :-future], test_target)
         print('test loss:', loss.data.numpy()[0])
-        y = pred.data.numpy()
-        # draw the result
-        plt.figure(figsize=(30,10))
-        plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
-        plt.xlabel('x', fontsize=20)
-        plt.ylabel('y', fontsize=20)
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        def draw(yi, color):
-            plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
-            plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
-        draw(y[0], 'r')
-        draw(y[1], 'g')
-        draw(y[2], 'b')
-        plt.savefig('./gru_train/predict%d.pdf'%i)
-        plt.close()
+        # y = pred.data.numpy()
+        # # draw the result
+        # plt.figure(figsize=(30,10))
+        # plt.title('Predict future values for time sequences\n(Dashlines are predicted values)', fontsize=30)
+        # plt.xlabel('x', fontsize=20)
+        # plt.ylabel('y', fontsize=20)
+        # plt.xticks(fontsize=20)
+        # plt.yticks(fontsize=20)
+        # def draw(yi, color):
+        #     plt.plot(np.arange(input.size(1)), yi[:input.size(1)], color, linewidth = 2.0)
+        #     plt.plot(np.arange(input.size(1), input.size(1) + future), yi[input.size(1):], color + ':', linewidth = 2.0)
+        # draw(y[0], 'r')
+        # draw(y[1], 'g')
+        # draw(y[2], 'b')
+        # plt.savefig('predict%d.pdf'%i)
+        # plt.close()
